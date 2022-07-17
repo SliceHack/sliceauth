3afca403952bf0fcdd11d60f91dd38ffee6ee9e7
NickRest <nickrestemail@gmail.com>
2022-07-17 17:30:29 -0400

    e

--- routes/checkAuth.js
+++ routes/checkAuth.js
@@ -16,7 +16,7 @@ router.get('/:hwid', function(req, res, next){
     if (db.has(hwid)) {
 
         var admin = db.get("ZGpsZXYxMC4wYW1kNjQxMC4wV2luZG93cyAxMEM6XFVzZXJzXGRqbGV2QU1ENjQgRmFtaWx5IDIzIE1vZGVsIDggU3RlcHBpbmcgMiwgQXV0aGVudGljQU1EQU1ENjRBTUQ2NDIzMjM=") == "381914174407835660" 
-        || db.get("TmljazEwLjBhbWQ2NDEwLjBXaW5kb3dzIDExQzpcVXNlcnNcTmlja0ludGVsNjQgRmFtaWx5IDYgTW9kZWwgMTQxIFN0ZXBwaW5nIDEsIEdlbnVpbmVJbnRlbEFNRDY0QU1ENjQ2Ng==") == "" == "853392200078983182";
+        || db.get("TmljazEwLjBhbWQ2NDEwLjBXaW5kb3dzIDExQzpcVXNlcnNcTmlja0ludGVsNjQgRmFtaWx5IDYgTW9kZWwgMTQxIFN0ZXBwaW5nIDEsIEdlbnVpbmVJbnRlbEFNRDY0QU1ENjQ2Ng==") == "853392200078983182";
 
 
         var response = {
