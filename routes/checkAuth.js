2eeba09da17cebc3d89c9871212a8be597a904ac
NickRest <nickrestemail@gmail.com>
2022-07-17 17:30:04 -0400

    e

--- routes/checkAuth.js
+++ routes/checkAuth.js
@@ -15,7 +15,9 @@ router.get('/:hwid', function(req, res, next){
     const db = new JSONdb('accounts.json');
     if (db.has(hwid)) {
 
-        var admin = db.get(hwid) == "ZGpsZXYxMC4wYW1kNjQxMC4wV2luZG93cyAxMEM6XFVzZXJzXGRqbGV2QU1ENjQgRmFtaWx5IDIzIE1vZGVsIDggU3RlcHBpbmcgMiwgQXV0aGVudGljQU1EQU1ENjRBTUQ2NDIzMjM=" || db.get(hwid) == "853392200078983182";
+        var admin = db.get("ZGpsZXYxMC4wYW1kNjQxMC4wV2luZG93cyAxMEM6XFVzZXJzXGRqbGV2QU1ENjQgRmFtaWx5IDIzIE1vZGVsIDggU3RlcHBpbmcgMiwgQXV0aGVudGljQU1EQU1ENjRBTUQ2NDIzMjM=") == "381914174407835660" 
+        || db.get("TmljazEwLjBhbWQ2NDEwLjBXaW5kb3dzIDExQzpcVXNlcnNcTmlja0ludGVsNjQgRmFtaWx5IDYgTW9kZWwgMTQxIFN0ZXBwaW5nIDEsIEdlbnVpbmVJbnRlbEFNRDY0QU1ENjQ2Ng==") == "" == "853392200078983182";
+
 
         var response = {
             "status": "true",
